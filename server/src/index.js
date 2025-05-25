17cb857d66c1ead1f18432341e7b09a59499a8e7
rahul_r_432 <rahulramachandran432@gmail.com>
2025-05-25 14:21:58 +0530

    feat : database setup

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,7 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import { authRoute } from './routes/index.js'
+import { connectDB } from './config/db/database.js'
 
 dotenv.configDotenv()
 
@@ -12,16 +13,17 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-// routes
-
-
 
 const PORT = process.env.PORT
+
+// DB Connection
+connectDB()
 
 app.get('/',(req,res)=>{
     res.json({message:"Hey"})
 })
 
+// routes 
 app.use('/api/user',authRoute)
 
 app.listen(PORT, ()=>{
